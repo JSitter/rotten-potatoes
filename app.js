2fa7579502c2bbd733d9cbc3f3a5aa55090b0d30
Justin Sitter <justin@jaytria.com>
2017-09-22 08:47:27 -0700

    Clarified comments

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
+
+//Connect to rotten-potatoes database
 mongoose.connect('localhost/rotten-potatoes');
-//log mongo db connection errors to console
+
+//Log mongo db connection errors to console
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
 
-//setup Mongodb model
+//Setup Mongodb model
 var Review = mongoose.model('Review', {
   title: String
 });
 
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
+// Body parser to retrieve user post data
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Setup handlebars view engine and pass in parameters
+//Setup handlebars view engine and pass in setup parameters
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}));
 
-// CREATE review
+//Set handlebars view engine
+app.set('view engine', 'hbs');
+
+//Get review post data
 app.post('/reviews', function (req, res) {
   Review.create(req.body, function(err, review) {
     console.log(review);
@@ -28,8 +35,6 @@ app.post('/reviews', function (req, res) {
   })
 })
 
-app.set('view engine', 'hbs');
-
 // Home route
 app.get('/', function (req, res) {
   Review.find(function(err, reviews) {
@@ -37,11 +42,12 @@ app.get('/', function (req, res) {
   })
 })
 
+//Route for form to post new Reviews
 app.get('/reviews/new', function (req, res) {
   res.render('reviews-new', {});
 })
 
-
+//Listen on port 30010
 app.listen(30010, function () {
-  console.log('Portfolio App listening on port 3000!');
+  console.log('Rotten Potatoes listening on port 3000!');
 });
