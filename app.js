39c947952818a0e0b15a877c3169add6090e2310
Justin Sitter <justin@jaytria.com>
2017-09-21 21:53:24 -0700

    Mongo db works

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
-mongoose.connect('mongod://localhost/rotten-potatoes');
+mongoose.connect('localhost/rotten-potatoes');
+//log mongo db connection errors to console
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
+
+//setup Mongodb model
 var Review = mongoose.model('Review', {
   title: String
 });
@@ -12,39 +16,32 @@ var Review = mongoose.model('Review', {
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//Setup handlebars view engine and pass in parameters
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: 'hbs'}));
 
+// CREATE review
 app.post('/reviews', function (req, res) {
-  Review.create(req.body, function(err, review){
-    console.log(review)
-    res.redirect('/')
+  Review.create(req.body, function(err, review) {
+    console.log(review);
+
+    res.redirect('/');
   })
 })
 
 app.set('view engine', 'hbs');
 
-// var reviews = [
-//   { title: "Great Review"},
-//   { title: "Next Review"}
-// ];
-
-// app.get('/', function (req, res) {
-//   res.render('home', {msg: 'Hello World!'});
-
-// });
-
-app.get('/', function(req, res){
-  Review.find(function(err, reviews){
+// Home route
+app.get('/', function (req, res) {
+  Review.find(function(err, reviews) {
     res.render('reviews-index', {reviews: reviews});
   })
-  
-});
+})
 
 app.get('/reviews/new', function (req, res) {
   res.render('reviews-new', {});
 })
 
 
-app.listen(3000, function () {
+app.listen(30010, function () {
   console.log('Portfolio App listening on port 3000!');
 });
